a5e438fa4605b4c96013879a3c2c2140a9954cac
Farzana E <119516397+farzana039@users.noreply.github.com>
2025-01-26 14:36:08 +0530

    Add files via upload

---
+++ Comparision.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a"))
+b=int(input("Enter b"))
+#using ternary operators to print true or false for the conditions
+print(f"{a} == {b} :{a==b}")
+print(f"{a} <= {b}: {a <= b}")
+print(f"{a} > {b}: {a > b}")
+print(f"{a} != {b}: {a != b}")
+print(f"{a} < {b}: {a < b}")
+print(f"{a} >= {b}: {a >= b}")
+
+
