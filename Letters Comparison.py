fcd9e405b70c38b415b3b06316e9299b09933336
Farzana E <119516397+farzana039@users.noreply.github.com>
2025-01-27 18:47:16 +0530

    Add files via upload

---
+++ Letters Comparison.py
@@ -0,0 +1,10 @@
+Firstletter =input("Enter letter 1")
+Secondletter =input("Enter letter 2")
+print(f"Is '{Firstletter}' < '{Secondletter}'? {Firstletter < Secondletter}")
+print(f"Is '{Firstletter}' <= '{Secondletter}'? {Firstletter <= Secondletter}")
+print(f"Is '{Firstletter}' > '{Secondletter}'? {Firstletter > Secondletter}")
+print(f"Is '{Firstletter}' >= '{Secondletter}'? {Firstletter >= Secondletter}")
+print(f"Is '{Firstletter}' == '{Secondletter}'? {Firstletter == Secondletter}")
+print(f"Is '{Firstletter}' != '{Secondletter}'? {Firstletter != Secondletter}")
+
+
